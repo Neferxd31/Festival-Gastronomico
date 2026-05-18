e5b6c642760bd1b7d4c53cbd361e4064e1e5af19
camilo2212 <juancamilouribemendoza@gmail.com>
2026-05-18 14:54:40 -0500

    eliminar voto

--- backend/interaccionapp/urls.py
+++ backend/interaccionapp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('<int:restaurante_id>/votar/', views.votar_restaurante, name='votar-restaurante'),
+    path('<int:restaurante_id>/eliminar-voto/', views.eliminar_voto, name='eliminar-voto'),
+    path('<int:restaurante_id>/verificar-voto/', views.verificar_voto, name='verificar-voto'),
 ]
